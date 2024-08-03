9e043023f499ba0b9be5e18432105352898111b5
EmmanuelOmorodion <omorodionemmanuel254@gmail.com>
2024-08-03 18:17:03 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let noun = "Emmanuel";                 
+let numericValue = 5;                   
+let calculatedNumber = numericValue * 4
+let isSunny = true;                    
+let schoolName = "Bard High School";   
+let story = `${noun} was at ${schoolName} on ${isSunny ? "a sunny" : "a cloudy"} day. ` +
+            `During the basketball game, with only ${calculatedNumber} seconds left on the clock, ` +
+            `${noun} made an amazing dunk over his teammate. ` +
+            `The crowd went silent, and it became the highlight of the game!`;
+console.log()
