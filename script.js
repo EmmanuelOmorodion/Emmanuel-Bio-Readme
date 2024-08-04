16fb7cb442c1504d9f367489e328743e1a0a5ec1
EmmanuelOmorodion <omorodionemmanuel254@gmail.com>
2024-08-03 20:26:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let noun = "Emmanuel";                 
-let numericValue = 5;                   
-let calculatedNumber = numericValue * 4
-let isSunny = true;                    
-let schoolName = "Bard High School";   
-let story = `${noun} was at ${schoolName} on ${isSunny ? "a sunny" : "a cloudy"} day. ` +
+ noun = "Emmanuel";                 
+ numericValue = 5;                   
+ calculatedNumber = numericValue * 4
+isSunny = true;                    
+schoolName = "Bard High School";   
+ story = `${noun} was at ${schoolName} on ${isSunny ? "a sunny" : "a cloudy"} day. ` +
             `During the basketball game, with only ${calculatedNumber} seconds left on the clock, ` +
             `${noun} made an amazing dunk over his teammate. ` +
             `The crowd went silent, and it became the highlight of the game!`;
